a48d41dbb0e50f376f2c059188410840640419a9
IsaacEspinoza91 <isaac.espinoza.b@usach.cl>
2025-03-29 23:57:07 -0300

    Add proyecto inicializado de Spring

---
+++ AppKartingRM/src/main/java/com/kartingRM/AppKartingRM/AppKartingRmApplication.java
@@ -0,0 +1,13 @@
+package com.kartingRM.AppKartingRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppKartingRmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppKartingRmApplication.class, args);
+	}
+
+}
---
+++ AppKartingRM/src/test/java/com/kartingRM/AppKartingRM/AppKartingRmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kartingRM.AppKartingRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppKartingRmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
